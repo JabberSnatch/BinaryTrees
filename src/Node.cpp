66cbb9e4877cc1c544ed51e86a7c8b51236b243b
Compte etudiant <etudiant@localhost.localdomain>
2015-04-07 11:36:06 +0200

    correction tab

--- src/Node.cpp
+++ src/Node.cpp
@@ -25,51 +25,46 @@ Node::Node(int data)
 
 bool 
 Node::check(){
-	if(_parent!=nullptr)
-		return false;
-	else{
-		if((_left==nullptr)&&(_right==nullptr))
-			return true;
-		else{
-			if(_left!=nullptr){
-				if(_left->getParent() != this)
-					return false;
-			}
-			if(_right!=nullptr){
-				if(_right->getParent() != this)
-					return false;
-			}
-			return (_right->nodeCheck()&&_left->nodeCheck());
-		}
-	}
+    if(_parent!=nullptr)
+        return false;
+    else{
+        if((_left==nullptr)&&(_right==nullptr))
+            return true;
+        else{
+            if(_left!=nullptr){
+                if(_left->getParent() != this)
+                    return false;
+            }
+            if(_right!=nullptr){
+                if(_right->getParent() != this)
+                    return false;
+            }
+            return (_right->nodeCheck()&&_left->nodeCheck());
+        }
+    }
 }
 
 
 bool 
 Node::nodeCheck(){
-	if((_left==nullptr)&&(_right==nullptr))
-		return true;
-	else{
-		bool leftChecked=true,rightChecked=true;
-		if(_left!=nullptr){
-			if(_left->getParent()!=this)
-				return false;
-			leftChecked=_left->nodeCheck();
-		}
-		if(_right!=nullptr){
-			if(_right->getParent()!=this)
-				return false;
-			rightChecked=_right->nodeCheck();
-		}
-		return ( rightChecked && leftChecked );
-	}
+    if((_left==nullptr)&&(_right==nullptr))
+        return true;
+    else{
+        bool leftChecked=true,rightChecked=true;
+        if(_left!=nullptr){
+            if(_left->getParent()!=this)
+                return false;
+            leftChecked=_left->nodeCheck();
+        }
+        if(_right!=nullptr){
+            if(_right->getParent()!=this)
+                return false;
+            rightChecked=_right->nodeCheck();
+        }
+        return ( rightChecked && leftChecked );
+    }
 }
 
-Node *  
-Node::getParent(){
-	return _parent;
-	
-}
 
 void 
 Node::degraph()
@@ -126,6 +121,12 @@ bool
 Node::isRightFree()
 {
     return (_right == nullptr);
+}
+
+Node *  
+Node::_getParent(){
+	return _parent;
+	
 }
 
 void
