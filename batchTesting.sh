5add8d6aa17606b45cfc659ad8d72b75c8a1ac1a
JabberSnatch <mr.narwall@gmail.com>
2015-06-03 18:06:23 +0200

    Added a first line recording the tests being done

--- batchTesting.sh
+++ batchTesting.sh
@@ -3,14 +3,18 @@
 test1="DLIST"
 test2="SLIST"
 
-taille=8
+taille=2
 increment=2
-tailleMax=10000
+tailleMax=1000
+
+nbTest=10
+
+echo "n;$test1;$test2;"
 
 while [ $taille -le $tailleMax ]
 do
-    ./bin/prog $test1 $test2 -F $taille -v
-    taille=$((taille * increment))
+    ./bin/prog $test1 $test2 -F $taille -v -C $nbTest
+    taille=$((taille + increment))
 done
 
 
